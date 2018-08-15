f4ddbd4b2052e5072e5e6372e7f37c2be3fd9f61
Prathmesh Palande <palande1996@gmail.com>
2018-08-15 14:13:16 +0530

    Added the name of the other party in getPassbook()

--- src/main/java/com/mini/japa/db/operators/PassbookOperator.java
+++ src/main/java/com/mini/japa/db/operators/PassbookOperator.java
@@ -1,6 +1,5 @@
 package com.mini.japa.db.operators;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -14,48 +13,66 @@ import java.util.Map;
 public class PassbookOperator {
 
 	public Map<Integer, List<String>> getPassbook(String phone) {
-		
+
 		Map<Integer, List<String>> response = new HashMap<Integer, List<String>>();
-		
+
+		Map<String, String> namesMap = new HashMap<String, String>();
+
 		Connection conn = null;
 		try {
 
 			Class.forName("com.mysql.jdbc.Driver");
-//			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/upaydb", "root", "admin");
-			conn = DriverManager.getConnection("jdbc:mysql://upay.cuadni5olhbe.us-east-2.rds.amazonaws.com:3306/upaydb", "upay", "rusprapalosw$");
+			conn = DriverManager.getConnection("jdbc:mysql://upay.cuadni5olhbe.us-east-2.rds.amazonaws.com:3306/upaydb",
+					"upay", "rusprapalosw$");
 			
-			String query = "select * from transactionhistory where fromphone = ? or tophone = ?";
-			
-			PreparedStatement transactionHistoryPreparedStatement = conn.prepareStatement(query);
-			
+			populateFromName(conn, phone, namesMap);
+			populateToName(conn, phone, namesMap);
+
+			String queryPassbook = "select * from transactionhistory where fromphone = ? or tophone = ?";
+
+			PreparedStatement transactionHistoryPreparedStatement = conn.prepareStatement(queryPassbook);
+
 			transactionHistoryPreparedStatement.setString(1, phone);
 			transactionHistoryPreparedStatement.setString(2, phone);
-			
-			ResultSet rs = transactionHistoryPreparedStatement.executeQuery();
-			
-			while(rs.next()) {
-				
-				int key = rs.getInt(1);
-				
-				List<String> valueList = new ArrayList<String>();
-				
-				valueList.add(rs.getString(2));
-				valueList.add(rs.getString(3));
-				valueList.add("" + rs.getDouble(4));
-				valueList.add("" + rs.getObject(5));
-				
-				response.put(key, valueList);
-				
+
+			ResultSet rsPassbook = transactionHistoryPreparedStatement.executeQuery();
+
+			while (rsPassbook.next()) {
+
+					int key = rsPassbook.getInt(1);
+
+					List<String> valueList = new ArrayList<String>();
+
+					String fromPhone = rsPassbook.getString(2);
+					String toPhone = rsPassbook.getString(3);
+
+					valueList.add(rsPassbook.getString(2));
+					valueList.add(rsPassbook.getString(3));
+					valueList.add("" + rsPassbook.getDouble(4));
+					valueList.add("" + rsPassbook.getObject(5));
+
+					String nameToBeAdded = "Error: Name could not be fetched!";
+					if (fromPhone.equals(phone))
+						nameToBeAdded = toPhone;
+					else
+						nameToBeAdded = fromPhone;
+
+					valueList.add(namesMap.get(nameToBeAdded));
+
+					response.put(key, valueList);
 			}
 			
+			transactionHistoryPreparedStatement.close();
+			rsPassbook.close();
+
 		} catch (SQLException e) {
-			System.out.println("Error Code: " + e.getErrorCode());
+			System.out.println("Error Code: " + e.getStackTrace());
 			System.out.println(e.getMessage());
+			e.printStackTrace();
 			ArrayList<String> errorList = new ArrayList<String>();
 			errorList.add("Error");
 			response.put(e.getErrorCode(), errorList);
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
 			try {
@@ -65,39 +82,91 @@ public class PassbookOperator {
 			} catch (SQLException ex) {
 				System.out.println("Error Code: " + ex.getErrorCode());
 				System.out.println(ex.getMessage());
+				ex.printStackTrace();
 				ArrayList<String> errorList = new ArrayList<String>();
 				errorList.add("Error");
 				response.put(ex.getErrorCode(), errorList);
 			}
 		}
-		
+
 		return response;
-		
+
 	}
-	
-	public double getBalance(String phone) {
+
+	public boolean populateFromName(Connection conn, String phone, Map<String, String> namesMap) {
+
+		String queryFromName = "select distinct(u.u_name) as u_name_f, phone\n"
+				+ "from users as u, transactionhistory as t\n"
+				+ "where t.tophone not like ? and u.phone = t.fromphone;";
+
+		try {
+
+			PreparedStatement fromNamePreparedStatement = conn.prepareStatement(queryFromName);
+
+			fromNamePreparedStatement.setString(1, phone);
+
+			ResultSet rsFromName = fromNamePreparedStatement.executeQuery();
+
+			while (rsFromName.next())
+				namesMap.put(rsFromName.getString(2), rsFromName.getString(1));
+			
+			fromNamePreparedStatement.close();
+			rsFromName.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return true;
+	}
+
+	public boolean populateToName(Connection conn, String phone, Map<String, String> namesMap) {
 		
+		try {
+			String queryToName = "select distinct(u.u_name) as u_name_t, phone\n"
+					+ "from users as u, transactionhistory as t\n"
+					+ "where t.fromphone not like ? and u.phone = t.tophone;";
+
+			PreparedStatement toNamePreparedStatement = conn.prepareStatement(queryToName);
+
+			toNamePreparedStatement.setString(1, phone);
+
+			ResultSet rsToName = toNamePreparedStatement.executeQuery();
+
+			while (rsToName.next())
+				namesMap.put(rsToName.getString(2), rsToName.getString(1));
+			
+			toNamePreparedStatement.close();
+			rsToName.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return true;
+	}
+
+	public double getBalance(String phone) {
+
 		Connection conn = null;
 		double response = -1;
-		
+
 		try {
 
 			Class.forName("com.mysql.jdbc.Driver");
-//			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/upaydb", "root", "admin");
-			conn = DriverManager.getConnection("jdbc:mysql://upay.cuadni5olhbe.us-east-2.rds.amazonaws.com:3306/upaydb", "upay", "rusprapalosw$");
-			
+			conn = DriverManager.getConnection("jdbc:mysql://upay.cuadni5olhbe.us-east-2.rds.amazonaws.com:3306/upaydb",
+					"upay", "rusprapalosw$");
+
 			String query = "select balance from users where phone = ?";
-			
+
 			PreparedStatement transactionHistoryPreparedStatement = conn.prepareStatement(query);
-			
+
 			transactionHistoryPreparedStatement.setString(1, phone);
-			
+
 			ResultSet rs = transactionHistoryPreparedStatement.executeQuery();
-			
+
 			rs.next();
-			
+
 			response = rs.getDouble(1);
-			
+
 		} catch (SQLException e) {
 			System.out.println("Error Code: " + e.getErrorCode());
 			System.out.println(e.getMessage());
@@ -105,7 +174,6 @@ public class PassbookOperator {
 			errorList.add("Error");
 			response = -1;
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
 			try {
@@ -120,9 +188,9 @@ public class PassbookOperator {
 				response = -1;
 			}
 		}
-		
+
 		return response;
-		
+
 	}
-	
+
 }
