c277f400bcf524af61cfcea04efa887dfc9ab3cd
Prathmesh Palande <palande1996@gmail.com>
2018-10-04 12:34:51 +0530

    Added forgot_password and getprofile API - v1.4.1

--- src/main/java/com/mini/japa/db/operators/UserOperator.java
+++ src/main/java/com/mini/japa/db/operators/UserOperator.java
@@ -5,6 +5,9 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserOperator {
 
@@ -163,10 +166,152 @@ public class UserOperator {
 		
 	}
 	
-	public Object getProfile(String phone) {
+	public Map<String, Object> getProfile(String phone) {
 		
-		return true;
+		int responseCode = 0;
+		String responseMessage = "Some error occured! Please check the tomcat log for more details.";
+		Map<String, Object> response = new HashMap<String, Object>();
+		Map<String, Object> responseData = new HashMap<String, Object>();
+		
+		Connection conn = null;
+		try {
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+			conn = DriverManager.getConnection("jdbc:mysql://upay.cuadni5olhbe.us-east-2.rds.amazonaws.com:3306/upaydb", "upay", "rusprapalosw$");
+
+			System.out.println("Connection to SQLite has been established.");
+			
+			String querySelectProfile = "select u_name, aadhaar, email, phone, dob, balance from users where phone = ?";
+			
+			PreparedStatement preparedStatementSelectProfile = conn.prepareStatement(querySelectProfile);
+			preparedStatementSelectProfile.setString(1, phone);
+			
+			ResultSet resultSetSelectProfile = preparedStatementSelectProfile.executeQuery();
+			
+			if(resultSetSelectProfile.next()) {
+				
+				String userName = resultSetSelectProfile.getString(1);
+				String aadhaar = resultSetSelectProfile.getString(2);
+				String email = resultSetSelectProfile.getString(3);
+				String dbPhone = resultSetSelectProfile.getString(4);
+				String dob = resultSetSelectProfile.getString(5);
+				double balance = resultSetSelectProfile.getDouble(6);
+				
+				responseCode = 1;
+				responseData.put("u_name", userName);
+				responseData.put("aadhaar", aadhaar);
+				responseData.put("email", email);
+				responseData.put("phone", dbPhone);
+				responseData.put("dob", dob);
+				responseData.put("balance", balance);
+				
+				response.put("response_code", responseCode);
+				response.put("response_data", responseData);
+			}
+			
+		} catch (SQLException e) {
+			System.out.println("Error Code: " + e.getErrorCode());
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		} finally {
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException ex) {
+				System.out.println("Error Code: " + ex.getErrorCode());
+				System.out.println(ex.getMessage());
+				ex.printStackTrace();
+			}
+		}
+		
+		if(responseCode == 0) {
+			response.put("response_code", responseCode);
+			responseData.put("response_message", responseMessage);
+			response.put("response_data", responseData);
+		}
+		
+		return response;
 		
 	}
 
+	public Map<String, Object> resetPassword(String phone, String oldPassword, String newPassword) {
+		
+		int responseCode = 0;
+		String responseMessage = "Some error occured! Please check the tomcat log for more details.";
+		Map<String, Object> response = new HashMap<String, Object>();
+		Map<String, Object> responseData = new HashMap<String, Object>();
+		
+		Connection conn = null;
+		try {
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+			conn = DriverManager.getConnection("jdbc:mysql://upay.cuadni5olhbe.us-east-2.rds.amazonaws.com:3306/upaydb", "upay", "rusprapalosw$");
+
+			System.out.println("Connection to SQLite has been established.");
+			
+			String querySelectPassword = "select password from users where phone = ?";
+			
+			PreparedStatement preparedStatementSelectPassword = conn.prepareStatement(querySelectPassword);
+			preparedStatementSelectPassword.setString(1, phone);
+			
+			ResultSet resultSetSelectQuery = preparedStatementSelectPassword.executeQuery();
+			
+			if(resultSetSelectQuery.next() ) {
+				
+				String dbPassword = resultSetSelectQuery.getString(1);
+				
+				if(oldPassword.equals(dbPassword)) {
+					
+					String queryUpdatePassword = "update users set password = ? where phone = ?";
+					
+					PreparedStatement preparedStatementUpdatePassword = conn.prepareStatement(queryUpdatePassword);
+					preparedStatementUpdatePassword.setString(1, newPassword);
+					preparedStatementUpdatePassword.setString(2, phone);
+					
+					preparedStatementUpdatePassword.execute();
+					
+					responseCode = 1;
+					responseMessage = "Password Reset Successfully! You may now proceed to login again.";
+				}
+				else {
+					responseCode = 0;
+					responseMessage = "Incorrect Old Password! Please enter the correct password and try again.";
+				}
+				
+			}
+			else {
+				responseCode = 0;
+				responseMessage = "User does not exist! Please check the phone number and try again.";
+			}
+
+		} catch (SQLException e) {
+			System.out.println("Error Code: " + e.getErrorCode());
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		} finally {
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException ex) {
+				System.out.println("Error Code: " + ex.getErrorCode());
+				System.out.println(ex.getMessage());
+				ex.printStackTrace();
+			}
+		}
+		
+		response.put("response_code", responseCode);
+		responseData.put("response_message", responseMessage);
+		response.put("response_data", responseData);
+		
+		return response;
+		
+	}
 }
